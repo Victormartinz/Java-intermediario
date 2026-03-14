2d02a5e6b2a8d014910b90872d69bb17d6616a40
VictorMs <martinssantosvictor@gmail.com>
2026-03-14 19:56:12 -0300

    comecei aprender como funcionar metodos em POO no Java

--- JavaIntermediario/src/Main.java
+++ JavaIntermediario/src/Main.java
@@ -1,15 +1,28 @@
 public class Main {
     public static void main(String[] args) {
 
+        //------------Naruto--------------
+
         Ninja Naruto = new Ninja();
         Naruto.nome = "Naruto Uzumaki";
         Naruto.idade = 18;
         Naruto.aldeia = "Konoha";
 
+        //-----------Sasuke------------------
+
         Ninja Sasuke = new Ninja();
         Sasuke.nome = "Sasuke Uchiha";
         Sasuke.idade = 19;
         Sasuke.aldeia = "Konoha";
+
+        String chamadaSasuke = Sasuke.BoasVindas();
+        System.out.println(chamadaSasuke);
+        Sasuke.SharinganAtivado();
+
+        int quantoTempo = Sasuke.AnosParaSerHokage(70);
+        System.out.println("Você tem " + Sasuke.idade + " Anos," + " falta no minimo " + quantoTempo + " Anos " + "para ser hokage");
+
+        //------------Sakura---------------------
 
         Ninja Sakura = new Ninja();
         Sasuke.nome = "Sakura Haruno";
--- JavaIntermediario/src/Ninja.java
+++ JavaIntermediario/src/Ninja.java
@@ -4,4 +4,16 @@ public class Ninja {
     String aldeia;
     int idade;
 
+    public void SharinganAtivado(){
+        System.out.println("O Sharingar Ativou");
+    }
+
+    public String BoasVindas(){
+        return "Oi, Meu nome é Sasuke e eu sou um Ninja Vingador";
+    }
+
+    public int AnosParaSerHokage(int idadeMininaParaSerHokage){
+        return idadeMininaParaSerHokage - idade;
+    }
+
 }
